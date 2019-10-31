49278848c1c8c7943aa1fcef54b2106fcee6b3ec
Max Ieremenko <max.ieremenko@gmail.com>
2019-10-31 14:24:39 +0100

    validate command: double-check status AutomaticallyApproved

--- Sources/ThirdPartyLibraries.Suite/Commands/ValidateCommandState.cs
+++ Sources/ThirdPartyLibraries.Suite/Commands/ValidateCommandState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace ThirdPartyLibraries.Suite.Commands
             AppName = appName;
 
             _appPackages = new HashSet<LibraryId>();
-            _infoById = new Dictionary<LibraryId, (string LicenseCode, PackageApprovalStatus Status)>();
+            _infoById = new Dictionary<LibraryId, (string, PackageApprovalStatus)>();
         }
 
         public IPackageRepository Repository { get; }
@@ -29,11 +30,23 @@ namespace ThirdPartyLibraries.Suite.Commands
         public async Task InitializeAsync(CancellationToken token)
         {
             var libraries = await Repository.Storage.GetAllLibrariesAsync(token);
+            var licenseRequiresApprovalByCode = new Dictionary<string, bool>(StringComparer.OrdinalIgnoreCase);
+
             foreach (var id in libraries)
             {
                 var package = await Repository.LoadPackageAsync(id, token);
+                
+                var approvalStatus = package.ApprovalStatus;
+                if (approvalStatus == PackageApprovalStatus.AutomaticallyApproved)
+                {
+                    var requiresApproval = await LicenseRequiresApprovalAsync(package.LicenseCode, licenseRequiresApprovalByCode, token);
+                    if (requiresApproval)
+                    {
+                        approvalStatus = PackageApprovalStatus.HasToBeApproved;
+                    }
+                }
 
-                _infoById.Add(id, (package.LicenseCode, package.ApprovalStatus));
+                _infoById.Add(id, (package.LicenseCode, approvalStatus));
 
                 if (!package.SourceCode.Equals(PackageSources.Custom) && package.UsedBy.Any(i => i.EqualsIgnoreCase(AppName)))
                 {
@@ -64,6 +77,34 @@ namespace ThirdPartyLibraries.Suite.Commands
         public IEnumerable<LibraryId> GetAppTrash()
         {
             return _appPackages;
+        }
+
+        private async ValueTask<bool> LicenseRequiresApprovalAsync(string licenseExpression, IDictionary<string, bool> cache, CancellationToken token)
+        {
+            if (licenseExpression.IsNullOrEmpty())
+            {
+                return true;
+            }
+
+            var codes = LicenseExpression.GetCodes(licenseExpression);
+
+            var result = false;
+            foreach (var code in codes)
+            {
+                if (!cache.TryGetValue(code, out var requiresApproval))
+                {
+                    var index = await Repository.Storage.ReadLicenseIndexJsonAsync(code, token);
+                    requiresApproval = index == null || index.RequiresApproval;
+                    cache.Add(code, requiresApproval);
+                }
+
+                if (requiresApproval)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
         }
     }
 }
