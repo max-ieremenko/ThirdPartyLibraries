42a129ff470b92862c3ab2c98376c0162760ae91
max-ieremenko <>
2022-05-14 11:43:22 +0200

    ThirdPartyNotices fill package.PackageLicense

--- Sources/ThirdPartyLibraries.Suite.Test/Commands/GenerateCommandTest.cs
+++ Sources/ThirdPartyLibraries.Suite.Test/Commands/GenerateCommandTest.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
 using Shouldly;
@@ -73,14 +74,10 @@ public class GenerateCommandTest
                 return Task.FromResult(package);
             });
 
-        var serviceProvider = new Mock<IServiceProvider>(MockBehavior.Strict);
-        serviceProvider
-            .Setup(p => p.GetService(typeof(IPackageRepository)))
-            .Returns(_packageRepository.Object);
-        serviceProvider
-            .Setup(p => p.GetService(typeof(ILogger)))
-            .Returns(_logger.Object);
-        _serviceProvider = serviceProvider.Object;
+        var services = new ServiceCollection();
+        services.AddSingleton(_packageRepository.Object);
+        services.AddSingleton(_logger.Object);
+        _serviceProvider = services.BuildServiceProvider();
 
         _sut = new GenerateCommand
         {
@@ -100,6 +97,7 @@ public class GenerateCommandTest
     {
         var package = new Package
         {
+            SourceCode = PackageSources.NuGet,
             Name = "Newtonsoft.Json",
             Version = "12.0.2",
             LicenseCode = "MIT",
@@ -155,6 +153,7 @@ public class GenerateCommandTest
     {
         var package = new Package
         {
+            SourceCode = PackageSources.NuGet,
             Name = "Newtonsoft.Json",
             Version = "12.0.2",
             LicenseCode = "MIT",
@@ -178,6 +177,10 @@ public class GenerateCommandTest
             .Setup(s => s.OpenLicenseFileReadAsync("MIT", "index.json", CancellationToken.None))
             .ReturnsAsync(license.JsonSerialize);
 
+        _storage
+            .Setup(s => s.OpenLicenseFileReadAsync("MIT", "license.txt", CancellationToken.None))
+            .ReturnsAsync("MIT license text".AsStream);
+
         UpdateTemplate("with-lic-hrefs.txt");
 
         await _sut.ExecuteAsync(_serviceProvider, CancellationToken.None).ConfigureAwait(false);
@@ -199,10 +202,85 @@ public class GenerateCommandTest
     }
 
     [Test]
+    public async Task GenerateThirdPartyNoticesWithPackageLicenses()
+    {
+        var package = new Package
+        {
+            SourceCode = PackageSources.NuGet,
+            Name = "Newtonsoft.Json",
+            Version = "12.0.2",
+            LicenseCode = "MIT",
+            HRef = "https://www.nuget.org/packages/Newtonsoft.Json/12.0.2",
+            Author = "James Newton-King",
+            Copyright = "Copyright © James Newton-King 2008",
+            UsedBy = new[] { PackageApplication.Public(AppName) },
+            ThirdPartyNotices = "some extra notices",
+            Licenses =
+            {
+                new PackageLicense
+                {
+                    Code = "MIT",
+                    HRef = "https://licenses.nuget.org/MIT",
+                    Subject = PackageLicense.SubjectPackage
+                }
+            }
+        };
+        _packages.Add(package);
+
+        var license = new LicenseIndexJson
+        {
+            Code = "MIT",
+            HRef = "https://spdx.org/licenses/MIT.html",
+            FileName = "license.txt",
+            FullName = "MIT License"
+        };
+
+        _storage
+            .Setup(s => s.OpenLicenseFileReadAsync("MIT", "index.json", CancellationToken.None))
+            .ReturnsAsync(license.JsonSerialize);
+
+        _storage
+            .Setup(s => s.OpenLicenseFileReadAsync("MIT", "license.txt", CancellationToken.None))
+            .ReturnsAsync("MIT license text".AsStream);
+
+        _storage
+            .Setup(s => s.FindLibraryFilesAsync(new LibraryId(package.SourceCode, package.Name, package.Version), "package-*lic*", CancellationToken.None))
+            .ReturnsAsync(() => new[] { "package-license.md" });
+
+        _storage
+            .Setup(s => s.OpenLibraryFileReadAsync(new LibraryId(package.SourceCode, package.Name, package.Version), "package-license.md", CancellationToken.None))
+            .ReturnsAsync(() => new MemoryStream("newtonsoft license text".AsBytes()));
+
+        UpdateTemplate("with-package-files.txt");
+
+        await _sut.ExecuteAsync(_serviceProvider, CancellationToken.None).ConfigureAwait(false);
+
+        FileAssert.Exists(Path.Combine(_to.Location, GenerateCommand.OutputFileName));
+
+        var output = File.ReadAllText(Path.Combine(_to.Location, GenerateCommand.OutputFileName));
+        output.ShouldContain(package.Name);
+        output.ShouldContain(package.Version);
+        output.ShouldContain(package.HRef);
+        output.ShouldContain(package.Author);
+        output.ShouldContain(package.Copyright);
+        output.ShouldContain(package.ThirdPartyNotices);
+
+        output.ShouldContain(package.Licenses[0].HRef);
+        output.ShouldContain("newtonsoft.json.md");
+        output.ShouldContain(license.FullName);
+
+        FileAssert.Exists(Path.Combine(_to.Location, "Licenses", "newtonsoft.json.md"));
+
+        output = File.ReadAllText(Path.Combine(_to.Location, "Licenses", "newtonsoft.json.md"));
+        output.ShouldBe("newtonsoft license text");
+    }
+
+    [Test]
     public async Task SkipPackage()
     {
         var package1 = new Package
         {
+            SourceCode = PackageSources.NuGet,
             Name = "internal package",
             Version = "1",
             UsedBy = new[] { PackageApplication.Internal(AppName) }
@@ -211,6 +289,7 @@ public class GenerateCommandTest
 
         var package2 = new Package
         {
+            SourceCode = PackageSources.NuGet,
             Name = "other application",
             Version = "1",
             UsedBy = new[] { PackageApplication.Public(AppName + "2") }
@@ -224,88 +303,6 @@ public class GenerateCommandTest
         var output = File.ReadAllText(Path.Combine(_to.Location, GenerateCommand.OutputFileName));
         output.ShouldNotContain(package1.Name);
         output.ShouldNotContain(package2.Name);
-    }
-
-    [Test]
-    public async Task CopyDependentLicense()
-    {
-        var package = new Package
-        {
-            Name = "name",
-            Version = "version",
-            LicenseCode = "LGPL-3.0",
-            UsedBy = new[] { PackageApplication.Public(AppName) }
-        };
-        _packages.Add(package);
-
-        var lgplLicense = new LicenseIndexJson
-        {
-            Code = "LGPL-3.0",
-            HRef = "https://spdx.org/licenses/LGPL-3.0",
-            FileName = "license.txt",
-            FullName = "GNU Lesser General Public License v3.0"
-        };
-
-        var gplLicense = new LicenseIndexJson
-        {
-            Code = "GPL-3.0",
-            HRef = "https://spdx.org/licenses/GPL-3.0",
-            FileName = "license.txt",
-            FullName = "GNU General Public License v3.0"
-        };
-
-        var mitLicense = new LicenseIndexJson
-        {
-            Code = "MIT",
-            HRef = "https://spdx.org/licenses/MIT.html",
-            FileName = "license.txt",
-            FullName = "MIT License"
-        };
-
-        lgplLicense.Dependencies = new[] { gplLicense.Code };
-        gplLicense.Dependencies = new[] { mitLicense.Code };
-        mitLicense.Dependencies = new[] { lgplLicense.Code };
-
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(lgplLicense.Code, "index.json", CancellationToken.None))
-            .ReturnsAsync(lgplLicense.JsonSerialize);
-
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(gplLicense.Code, "index.json", CancellationToken.None))
-            .ReturnsAsync(gplLicense.JsonSerialize);
-
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(mitLicense.Code, "index.json", CancellationToken.None))
-            .ReturnsAsync(mitLicense.JsonSerialize);
-            
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(lgplLicense.Code, "license.txt", CancellationToken.None))
-            .ReturnsAsync(() => new MemoryStream("LGPL-3.0 license text".AsBytes()));
-
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(gplLicense.Code, "license.txt", CancellationToken.None))
-            .ReturnsAsync(() => new MemoryStream("GPL-3.0 license text".AsBytes()));
-
-        _storage
-            .Setup(s => s.OpenLicenseFileReadAsync(mitLicense.Code, "license.txt", CancellationToken.None))
-            .ReturnsAsync(() => new MemoryStream("MIT license text".AsBytes()));
-
-        UpdateTemplate("with-lic-files.txt");
-
-        await _sut.ExecuteAsync(_serviceProvider, CancellationToken.None).ConfigureAwait(false);
-
-        FileAssert.Exists(Path.Combine(_to.Location, "Licenses", "MIT-license.txt"));
-        FileAssert.Exists(Path.Combine(_to.Location, "Licenses", "GPL-3.0-license.txt"));
-        FileAssert.Exists(Path.Combine(_to.Location, "Licenses", "LGPL-3.0-license.txt"));
-
-        var output = File.ReadAllText(Path.Combine(_to.Location, "Licenses", "MIT-license.txt"));
-        output.ShouldBe("MIT license text");
-
-        output = File.ReadAllText(Path.Combine(_to.Location, "Licenses", "LGPL-3.0-license.txt"));
-        output.ShouldBe("LGPL-3.0 license text");
-
-        output = File.ReadAllText(Path.Combine(_to.Location, "Licenses", "GPL-3.0-license.txt"));
-        output.ShouldBe("GPL-3.0 license text");
     }
 
     private void UpdateTemplate(string fileName)
