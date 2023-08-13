355da8f842d3062d46a04fccdf9456d3d05fa75e
max-ieremenko <max.ieremenko@gmail.com>
2023-08-13 15:43:15 +0200

    move HttpClientFactory to shared

---
+++ Sources/ThirdPartyLibraries.Suite/Configuration/SkipCertificateCheckConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ThirdPartyLibraries.Suite.Configuration;
+
+public sealed class SkipCertificateCheckConfiguration
+{
+    public const string SectionName = "skipCertificateCheck";
+
+    public SkipCertificateCheckConfiguration()
+    {
+        ForceLogRequest();
+    }
+
+    public string[] ByHost { get; set; } = Array.Empty<string>();
+
+    public bool LogRequest { get; set; }
+
+    [Conditional("DEBUG")]
+    private void ForceLogRequest()
+    {
+        LogRequest = true;
+    }
+}
