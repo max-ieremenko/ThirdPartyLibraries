6132da5db632ed639e1e1af024e325ca674268c6
max-ieremenko <max.ieremenko@gmail.com>
2025-02-08 14:38:29 +0100

    minimal pwsh version 7.2+

--- Sources/ThirdPartyLibraries.PowerShell/ThirdPartyLibraries.psm1
+++ Sources/ThirdPartyLibraries.PowerShell/ThirdPartyLibraries.psm1
@@ -1,13 +1,13 @@
 Set-StrictMode -Version Latest
 
-if (($PSVersionTable.PSEdition -ne "Core") -or ([version]$PSVersionTable.PSVersion -lt "7.0.0")) {
-    Write-Error "This module requires PowerShell 7.0.0+. Please, upgrade your PowerShell version."
+if (($PSVersionTable.PSEdition -ne 'Core') -or ([version]$PSVersionTable.PSVersion -lt '7.2.0')) {
+    Write-Error 'This module requires PowerShell 7.2+. Please, upgrade your PowerShell version.'
     Exit 1
 }
 
 $psModule = $ExecutionContext.SessionState.Module
 $root = $psModule.ModuleBase
-$dllPath = Join-Path -Path $root "ThirdPartyLibraries.PowerShell.dll"
+$dllPath = Join-Path -Path $root 'ThirdPartyLibraries.PowerShell.dll'
 
 $importedModule = Import-Module -Name $dllPath -PassThru
 
